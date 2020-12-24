b557583af3dba4e37da0dff54b3f0b4e0253be54
frederic boileau <fred.boileau@gmail.com>
2020-12-24 11:47:19 -0500

    lets get back to work

---
+++ .auctex-auto/whenaxiomsfall.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "whenaxiomsfall"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "textures"
+    "trees"
+    "article"
+    "art10"
+    "fontspec"
+    "titlesec"
+    "xcolor"
+    "comment"))
+ :latex)
+
